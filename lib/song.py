4ff195a04c40aefd29168e27b64cba73e1796304
williammaina <wmaina8442@gmail.com>
2026-06-07 02:15:06 +0300

    Implement Song class with song tracking and analytics

--- lib/song.py
+++ lib/song.py
@@ -1,39 +1,47 @@
 class Song:
+    # Class attributes (corrected 'artists_count' to 'artist_count' to pass the test)
     count = 0
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
+    artist_count = {}  # Fixed typo here
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
 
+        # Call class methods using the class reference or self
         Song.add_song_to_count()
-        Song.add_to_genres(self)
-        Song.add_to_artists(self)
-        Song.add_to_genre_count(self)
-        Song.add_to_artists_count(self)
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artist_count(self.artist)
 
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
     @classmethod
-    def add_to_genres(cls, song):
-        if song.genre not in cls.genres:
-            cls.genres.append(song.genre)
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
     @classmethod
-    def add_to_artists(cls, song):
-        if song.artist not in cls.artists:
-            cls.artists.append(song.artist)
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
     @classmethod
-    def add_to_genre_count(cls, song):
-        cls.genre_count[song.genre] = cls.genre_count.get(song.genre, 0) + 1
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
 
     @classmethod
-    def add_to_artists_count(cls, song):
-        cls.artists_count[song.artist] = cls.artists_count.get(song.artist, 0) + 1
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
